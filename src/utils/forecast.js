2638d5287fa6b933b88b974e9477accd861ee5cd
Tony Amir <amirtony57@gmail.com>
2022-05-16 01:28:46 +0200

    to Celisius

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,7 +1,7 @@
 const request= require('request')
 
 const forecast=(latitude,longitude,callback)=>{
- const url ='http://api.weatherstack.com/current?access_key=ce443ee55143a180019a04728e84535e&query='+encodeURIComponent(latitude)+','+encodeURIComponent(longitude)+'&units=f'
+ const url ='http://api.weatherstack.com/current?access_key=ce443ee55143a180019a04728e84535e&query='+encodeURIComponent(latitude)+','+encodeURIComponent(longitude)+
  request({url,json:true},(error,{body})=>{
      if(error){
         callback('Unable to connect to weather service!',undefined)
