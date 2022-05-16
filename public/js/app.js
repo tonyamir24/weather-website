b972422279ca0829474b63bccd3f489e0d65a6c8
Tony Amir <amirtony57@gmail.com>
2022-05-16 02:12:08 +0200

    to Celisius

--- public/js/app.js
+++ public/js/app.js
@@ -3,13 +3,14 @@ const weatherForm =document.querySelector('form')
 const search =document.querySelector('input')
 const meesageOne=document.querySelector('#message-1')
 const meesagetwo=document.querySelector('#message-2')
-
+const img=document.querySelector('img')
 
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     const location=search.value
     meesageOne.textContent='Loading...'
     meesagetwo.textContent=''
+    img.src=''
     fetch('/weather?address='+location).then((response)=>{
     
         response.json().then((data)=>{
@@ -19,6 +20,7 @@ weatherForm.addEventListener('submit',(e)=>{
             else{
                 meesageOne.textContent=data.location
                 meesagetwo.textContent=data.forecast
+                img.src="/img/download.jpg"
             }
             
         }) 
